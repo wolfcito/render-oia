eeb237a3f78973f93533af904a0bf54eaa35a796
Luis Fernando <guffenix@gmail.com>
2025-06-29 10:43:49 -0500

    chore: initialize HTML, CSS, and JS files for the client application

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+// client/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';  // Puedes crear este archivo en blanco o con estilos mínimos
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
